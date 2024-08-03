8f94463fea472f3da6ce68e31b4d170eb2344efe
Phongkiatr <phongkiat2548@gmail.com>
2024-08-03 22:52:49 +0700

    Initial commit for Hello World

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,10 +1,6 @@
-from flask import Flask
+# HelloWorld.py
+def main():
+    print("Hello, World!")
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    main()
